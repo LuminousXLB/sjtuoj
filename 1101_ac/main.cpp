3efd62c9ba01fd559164a1d5fc0cc1d6b4b165b9
Taraxacum <john980118@163.com>
2018-08-13 16:24:45 +0800

    1101 ac

--- 1101_ac/main.cpp
+++ 1101_ac/main.cpp
@@ -1,9 +1,7 @@
 // 1101. SuperXOR
-// #554591 正确 / 分数：100 / 时间：131ms / 内存：1076kb
+// #554592 正确 / 分数：100 / 时间：80ms / 内存：1080kb
 #include <iostream>
 using namespace std;
-
-short XOR_table[] = { 0, 1, 3, 6, 0, 5, 1, 8, 6, 5 };
 
 int main(int argc, char const* argv[])
 {
@@ -13,13 +11,12 @@ int main(int argc, char const* argv[])
 
     int T;
     cin >> T;
+
     for (int t = 0; t < T; ++t) {
-        int cnt[16][10] = { 0 };
         unsigned long long num;
         cin >> num;
 
         unsigned long long threshold = 0;
-
         unsigned long long tmp = num / 100;
         if (tmp > 0) {
             threshold = (tmp - 1) * 100 + 99;
@@ -27,6 +24,7 @@ int main(int argc, char const* argv[])
             threshold = 0;
         }
 
+        int cnt[16][10] = { 0 };
         for (unsigned long long k = num; k > threshold; k--) {
             unsigned long long tmp = k;
             for (short i = 0; tmp > 0; tmp /= 10, i++) {
